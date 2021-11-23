a9a174691e47b8fb4ee5e1c8fc405c62b64f1a88
Alex <alex.quilloux@student-cs.fr>
2021-11-23 22:20:43 +0100

    nft on chain

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -20,7 +20,7 @@ contract MyEpicNFT is ERC721URIStorage{
 
         _safeMint(msg.sender, newItemId);
 
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/AOUC");
+        _setTokenURI(newItemId, "data:application/json;base;ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUdKc1lXTnJPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0p3YVc1cklpQXZQZ29nSUNBZ1BIUmxlSFFnZUQwaU5UQWxJaUI1UFNJMU1DVWlJR05zWVhOelBTSmlZWE5sSWlCa2IyMXBibUZ1ZEMxaVlYTmxiR2x1WlQwaWJXbGtaR3hsSWlCMFpYaDBMV0Z1WTJodmNqMGliV2xrWkd4bElqNUJiR1Y0YjFKdmVXRnNVSEpwYm1ObFBDOTBaWGgwUGdvOEwzTjJaejQ9Igp9");
         console.log("NFT w/ ID %s has been minted by %s", newItemId, msg.sender);
 
         _tokenIds.increment();
